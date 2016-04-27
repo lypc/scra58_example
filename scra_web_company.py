7536d7db800c323f8d47295ba382bcb6643a906b
lypc <717190776@qq.com>
2016-04-27 10:04:40 +0800

    lallalalalla

--- scra_web_company.py
+++ scra_web_company.py
@@ -1,9 +1,14 @@
 import requests,re,html
+import codecs
+
 from lxml import etree
 
 listing_url='http://m.58.com/sh/yewu/'
+listing_url='http://m.58.com/sh/yewu/'
 famouscompanyid='http://qy.58.com/mq/'
 # unfamouscompanyid=
+headers={'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1','From': 'http://m.58.com/sh' }
+
 
 tag=re.compile(r'.*<div\s*class\s*=\s*"myf"\s*>(.*)</div>.*')
 tagsplit=re.compile(r'<.*?>')
@@ -17,6 +22,8 @@ companywebaddre=re.compile(r'.*>(http.*?)<')
 famouscompanyre=re.compile(r'.*名企.*')
 
 liboxxpath="//ul[@class='list-info job']/li"
+liboxxpath="//div[@id='infolist']/dl"
+liboxxpath="//ul[@class='infoList']/li"
 nextxpath="//a[@class='pagenext']/@href"
 titlexpath="//span[@id='d_title']/text()"
 payxpath="//div[@class='price']/span[@class='pay']/text()"
@@ -124,19 +131,46 @@ def parserlisting(htmlstr,adf,companyf,count):
     page = etree.HTML(htmlstr)
     liboxs = page.xpath(liboxxpath)
     for libox in liboxs:
-        ad=dict()
+        # ad=dict()
         litree=etree.tostring(libox)
-        tags=re.split(tagsplit,tag.match(str(litree)).group(1))
-        for x in tags:
-            if spacere.match(x):
-              tags.remove(x)
-        ad['tag']=html.unescape('，'.join(tags).replace(' ',''))
-        ad['city']='sh'
-        ad['category']='yewu'
+        # tags=re.split(tagsplit,tag.match(str(litree)).group(1))
+        # for x in tags:
+        #     if spacere.match(x):
+        #       tags.remove(x)
+        # ad['tag']=html.unescape('，'.join(tags).replace(' ',''))
+        # ad['city']='sh'
+        # ad['category']='yewu'
         vadbox=etree.HTML(litree)
+        print(litree)
         # vadurl=etree.tostring(vadbox.xpath("//div[@class='title']/a/@href")[0])
-        vadurl=vadbox.xpath("//div[@class='title']/a/@href")[0]
-        vadr=requests.get(vadurl,headers={'referer': vadurl})
+        ad_url= vadbox.xpath("//div[@class='item-right']/a/@href")[0]
+        ad_title= vadbox.xpath("//div[@class='item-right']//dt/strong/text()")[0]
+        #company_url = vadbox.xpath("//dd/a[contains(@class,'fl')]/@href")[0]
+        company_name = vadbox.xpath("//div[@class='item-right']//dd[@class='info-desc']/em[2]/text()")
+        if len(company_name) == 0:
+            company_name = ['unknown']
+        company_name=company_name[0]
+        type = vadbox.xpath("//div[@class='item-last']/span")
+        if len(type) == 0:
+            type = ['unknown']
+        elif len(type) == 1:
+            type = ['unknown']
+            service = vadbox.xpath("//div[@class='item-last']/span/text()")
+        else:
+            type = vadbox.xpath("//div[@class='item-last']/span[1]/text()")
+            service = vadbox.xpath("//div[@class='item-last']/span[2]/text()")
+        type = type[0]
+        #area = vadbox.xpath("//dd[@class='w96']/text()")
+        #service = vadbox.xpath("//div[@class='item-last']/span[2]/text()")
+        if len(service) == 0:
+            #service = vadbox.xpath("//dd[@class='w68']/text()")
+            service = ['unknown']
+        service =service[0].strip()
+        s = '{},{},{},{}\n'.format( ad_title,company_name, type, service)#ad_url, ad_title, company_url, company_name,{},{},{},{},
+        print(s)
+        adf.write(s)
+        continue
+        vadr=requests.get(vadurl)
         vadtext=vadr.text
         ad['url']=vadr.url
         if not urlre.match(vadr.url):
@@ -148,23 +182,25 @@ def parserlisting(htmlstr,adf,companyf,count):
     while count<100:
         count=count+1
         try:
-            nexturl=page.xpath(nextxpath)[0]
+            nexturl=page.xpath(nextxpath)[0].split('?')[0]
         except Exception:
+            print('ohh')
             exit()
-        htmltext=requests.get('http://m.58.com'+nexturl).text
+        #htmltext=requests.get('http://m.58.com'+nexturl).text
+        htmltext=requests.get('http://sh.58.com'+nexturl,headers=headers).text
         print('http://m.58.com'+nexturl)
         parserlisting(htmltext,adf,companyf,count)
 
 
 
 count=1
-htmltext=requests.get(listing_url).text
-with open('ad.txt','w',errors='ignore')as adf:
+htmltext=requests.get(listing_url, headers=headers).text
+with codecs.open('adc.txt','w',"utf-8")as adf:
     with open('company.txt','w',errors='ignore')as companyf:
-        adf.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'%('id','url','城市','类目',
-                                                                          '标题','标签','薪酬','发布时间','查看次数','职位','要求',
-                                                                          '地点','福利','公司名','工作描述','电话','联系人'))
-        companyf.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'%('id','url','标题','是否名企','是否认证',
-                                                                         '类型','规模','行业','地址','介绍','电话',
-                                                                          '邮件','网址'))
+        #adf.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'%('id','url','城市','类目',
+        #                                                                  '标题','标签','薪酬','发布时间','查看次数','职位','要求',
+        #                                                                  '地点','福利','公司名','工作描述','电话','联系人'))
+        #companyf.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'%('id','url','标题','是否名企','是否认证',
+        #                                                                 '类型','规模','行业','地址','介绍','电话',
+        #                                                                  '邮件','网址'))
         parserlisting(htmltext,adf,companyf,count)
